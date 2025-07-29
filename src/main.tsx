a8820a4d793609c0f7f33399cd359d54cd0b8d95
Abid Ali <abidali129418@gmail.com>
2025-07-29 12:44:17 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { HelmetProvider } from 'react-helmet-async'; // <--- 1. Import this
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Wrap your App component with the HelmetProvider
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider> {/* <--- 2. Add the provider wrapper */}
+    <App />
+  </HelmetProvider>
+);
